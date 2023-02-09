5e67037acdad05a5fc8e6d595136df5dfd2596ec
Daniel Paul <40488825+danielpauldd@users.noreply.github.com>
2023-02-09 22:14:40 +0100

    Bucket Template manuell angewendet

---
+++ scripts/get-irfanview_reg_classes.ps1
@@ -0,0 +1,4 @@
+﻿Get-Item HKLM:SOFTWARE\Classes\Irfanview* | foreach { 
+    $regkey = $($_.Name).replace("HKEY_LOCAL_MACHINE","HKLM")
+    & "$env:COMSPEC" /c reg export $regkey D:\$(split-path $($_.Name) -leaf).reg | out-null
+}
