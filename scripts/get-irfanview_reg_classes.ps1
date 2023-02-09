6d2e84e1144e3ff9538cc0b8083adca575aff2b1
Daniel Paul <40488825+danielpauldd@users.noreply.github.com>
2023-02-09 22:23:08 +0100

    hilfsscripte entfernt

--- scripts/get-irfanview_reg_classes.ps1
+++
@@ -1,4 +0,0 @@
-﻿Get-Item HKLM:SOFTWARE\Classes\Irfanview* | foreach { 
-    $regkey = $($_.Name).replace("HKEY_LOCAL_MACHINE","HKLM")
-    & "$env:COMSPEC" /c reg export $regkey D:\$(split-path $($_.Name) -leaf).reg | out-null
-}
